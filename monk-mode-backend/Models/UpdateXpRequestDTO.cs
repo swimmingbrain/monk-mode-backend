2496f0b8fd52a04c589b8b3f8378692d42447531
Damaura21fhv <david.maurer@students.fhv.at>
2025-11-27 23:18:23 +0100

    reupload commit changes

--- monk-mode-backend/Models/UpdateXpRequestDTO.cs
+++ monk-mode-backend/Models/UpdateXpRequestDTO.cs
@@ -2,7 +2,7 @@
 
 namespace monk_mode_backend.Models {
     public class UpdateXpRequestDTO {
-        [Range(1, 10000, ErrorMessage = "XpToAdd must be between 1 and 10000.")]
+        [Range(1, 100000000, ErrorMessage = "XpToAdd must be between 1 and 10000.")]
         public int XpToAdd { get; set; }
     }
 }
